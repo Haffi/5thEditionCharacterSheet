b6e54a51776909c6fd2c6b495698e626dab1c882
Haffi <hafthor.bryndisarson@gmail.com>
2016-08-29 23:48:18 +0000

    fixing syntax

--- volen.js
+++ volen.js
@@ -44,7 +44,7 @@ var charJson =
         "animal handling",
         "athletics",
         "intimidation",
-        "perception",
+        "perception"
     ],
     "attacks": {
         "longsword": {
