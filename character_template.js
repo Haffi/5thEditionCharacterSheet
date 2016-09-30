7df7044dba0deb441f977ae5ec576afda6986c7e
Haffi <hafthor.bryndisarson@gmail.com>
2016-09-30 19:26:03 +0000

    adding a template for new characters

---
+++ character_template.js
@@ -0,0 +1,126 @@
+var charJson =
+{
+    "name": "Example Name",
+    "classes": {
+        "Sorcerer": {
+            "level": 1,
+            "hitdice": "d6"
+        },
+        "warlock": {
+            "level": 1,
+            "hitdice": "d8"
+        }
+    },
+    "inspirationpoints": 0,
+    "hitpoints": 10,
+    "ac": "10",
+    "speed": 10,
+    "mainattributes": {
+        "background": "Background",
+        "race": "Half-Human",
+        "alignment": "Good Bad",
+        "playername": "You",
+        "xp": null
+    },
+    "appearance": {
+        "age": 10,
+        "height": "5\'6\"",
+        "weight": "180 lbs",
+        "eyes": "Color",
+        "skin": "Color",
+        "hair": "Color"
+    },
+    "abilityscores": {
+        "str": 10,
+        "dex": 10,
+        "con": 10,
+        "int": 10,
+        "wis": 10,
+        "cha": 10
+    },
+    "savingthrow_proficiencies": [
+        "str", "dex"
+    ],
+    "skill_proficiences": [
+        "skill",
+        "skill",
+        "skill",
+        "skill"
+    ],
+    "attacks": {
+        "weapon": {
+            "ability": "dex",
+            "die": "1d6",
+            "proficient": true,
+            "type": "p",
+            "extraAttackMod": 1,
+            "extraDmgMod": 1
+        },
+        "weapon": {
+            "ability": "str",
+            "die": "1d10",
+            "proficient": true,
+            "type": "p"
+        }
+    },
+    "equipment": {
+        "coins": {
+            "copper": 0,
+            "silver": 0,
+            "electrum": 0,
+            "gold": 10,
+            "platinium": 0
+        },
+        "armor": [
+            "Armor",
+            "Shield"
+        ],
+        "other": [
+            "Backpack",
+            "Clothes"
+        ]
+    },
+    "personality": {
+        "traits": "Trait",
+        "ideals": "Ideal",
+        "bonds": "Bond",
+        "flaws": "Flaw"
+    },
+    "features": [
+        "Darkvision",
+        "Lucky"
+    ],
+    "spellcasting": {
+        "spellcastingAbility": "int",
+        "spellSlots": {
+            "spells0": 0,
+            "spells1": 0,
+            "spells2": 0,
+            "spells3": 0,
+            "spells4": 0,
+            "spells5": 0,
+            "spells6": 0,
+            "spells7": 0,
+            "spells8": 0,
+            "spells9": 0
+        },
+        "spells": {
+            "spells0": ["Spell", "Spellspell"],
+            "spells1": ["Spell"],
+            "spells2": ["Spell"],
+            "spells3": ["Spell"],
+            "spells4": ["Spell"],
+            "spells5": ["Spell"]
+        }
+    },
+    "proficiencies": [
+        "Thing",
+        "Tool",
+        "Weapons",
+        "Armors"
+    ],
+    "languages": [
+        "Common",
+        "Uncommon"
+    ]
+};
