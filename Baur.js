dee984c1b6599a66cc7b637b5affa94ee174313b
Haffi <hafthor.bryndisarson@gmail.com>
2016-09-30 22:39:46 +0000

    draft of Baur the Black

---
+++ Baur.js
@@ -0,0 +1,111 @@
+var charJson =
+{
+    "name": "Baur the Black",
+    "classes": {
+        "Wizard": {
+            "level": 1,
+            "hitdice": "d6"
+        }
+    },
+    "inspirationpoints": 0,
+    "hitpoints": 9,
+    "ac": "12",
+    "speed": 30,
+    "mainattributes": {
+        "background": "Wanderer", // Mixture of Outlander and Hermit
+        "race": "Half-Orc",
+        "alignment": "Neutral",
+        "playername": "Haffi",
+        "xp": 0
+    },
+    "appearance": {
+        "age": 25,
+        "height": "6\'6\"",
+        "weight": "197 lbs",
+        "eyes": "Black",
+        "skin": "Dark Grey",
+        "hair": "Black"
+    },
+    "abilityscores": {
+        "str": 10,
+        "dex": 14,
+        "con": 16,
+        "int": 15,
+        "wis": 13,
+        "cha": 8
+    },
+    "savingthrow_proficiencies": [
+        "int", "wis"
+    ],
+    "skill_proficiences": [
+        "Intimidation",
+        "Medicine",
+        "Survival",
+        "Arcana",
+        "Investigation"
+    ],
+    "attacks": {
+        "Dagger": {
+            "ability": "dex",
+            "die": "1d4",
+            "proficient": true,
+            "type": "p"
+        },
+        "Sling": {
+            "ability": "dex",
+            "die": "1d4",
+            "proficient": true,
+            "type": "b"
+        }
+    },
+    "equipment": {
+        "coins": {
+            "copper": 0,
+            "silver": 0,
+            "electrum": 0,
+            "gold": 5,
+            "platinum": 0
+        },
+        "armor": [
+            ],
+        "other": [
+            "Explorer\'s pack",
+            "Traveler\'s clothes",
+            "Winter blanket",
+            "Scroll case stuffed with notes on Medicine and Necromancy",
+            "Herbalism kit"
+        ]
+    },
+    "personality": {
+        "traits": "I am utterly serene, even in the face of disaster.",
+        "ideals": "Meddling in the affairs of others only causes trouble.",
+        "bonds": "My life\'s work is a series of tomes on Necromancy.",
+        "flaws": "I\'d risk too much to uncover a lost bit of knowledge."
+    },
+    "features": [
+        "Darkvision (60\')",
+        "Relentless Endurance",
+        "Savage Attacks",
+        "Arcane Recovery"
+    ],
+    "spellcasting": {
+        "spellcastingAbility": "int",
+        "spellSlots": {
+            "spells0": 0,
+            "spells1": 2
+        },
+        "spells": {
+            "spells0": ["Chill Touch", "Light", "Ray of Frost"],
+            "spells1": ["Ray of Sickness", "Identify"]
+        }
+    },
+    "proficiencies": [
+        "Herbalism kit",
+        "Tool"
+    ],
+    "languages": [
+        "Common",
+        "Orc",
+        "Elvish"
+    ]
+};
