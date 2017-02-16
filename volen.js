731029a6f023709cfbf2ca8d1a534d99af47475d
Haffi <hafthor.bryndisarson@gmail.com>
2017-02-16 20:55:23 +0000

    Update to lvl 5

--- volen.js
+++ volen.js
@@ -3,12 +3,12 @@ var charJson =
     "name": "Volen", // born and raised on the outskirts of a cattle ranch to the NNW of Longsaddle (scag p.48)
     "classes": { // father is unknown, mother (Aret) is a lone Orc shaman living in a small cottage
         "Fighter": { // Aret has come to some understanding with the Kromlor family who own the ranch
-            "level": 4, // Volen suspects her father is not a local, is curious to find out more about the outside world
+            "level": 5, // Volen suspects her father is not a local, is curious to find out more about the outside world
             "hitdice": "d10"
         }
     },
     "inspirationpoints": 0,
-    "hitpoints": 41, // 13 + 9 + 10 + 9
+    "hitpoints": 50, // 13 + 9 + 10 + 9 + 9
     "ac": 20, // 18 (plate) + 2 (shield)
     "speed": 30,
     "mainattributes": {
@@ -16,7 +16,7 @@ var charJson =
         "race": "Half-Orc",
         "alignment": "Lawful Neutral",
         "playername": "Haffi",
-        "xp": 2700 // page 15 in PHB
+        "xp": 6500 // page 15 in PHB
     },
     "appearance": {
         "age": 19, // left home at 14 years of age
@@ -67,6 +67,13 @@ var charJson =
             "die": "1d8",
             "proficient": true,
             "type": "bludgeoning"
+        },
+        "light crossbow": {
+            "ability": "dex",
+            "extraDmgMod": 0,
+            "die": "1d6",
+            "proficient": true,
+            "type": "piercing"
         }
     },
     "equipment": {
@@ -113,7 +120,8 @@ var charJson =
         "Mercenary Life: I have knowledge of and contacts within mercenary companies (scag p.152)",
         "Action Surge: Additional action once per short rest (p.72)",
         "Improved Critical: Score crit on 19 or 20 (p.72)",
-        "Resilient (Dex): Gail proficiency in Dexterity saving throws (p. 168)"
+        "Resilient (Dex): Gail proficiency in Dexterity saving throws (p. 168)",
+        "Extra attack: Attack twice with Attack action (p. 72)"
     ],
     "proficiencies": [ 
         "Dice games",
